1aaeaa1ffeb8d57aed4fcea25d13638279b06652
Nguyen Hoang Hai <nguyenhoanghai861@gmail.com>
2020-03-08 22:17:55 +0700

    .

--- js/script.js
+++ js/script.js
@@ -1,31 +1,41 @@
-// ;
-// (function ($) {
+;
+(function ($) {
 
-//     'use strict';
+    'use strict';
 
-//     var scrollTop = function () {
-//         window.scroll(function () {
-//             if (window.scrollTop() >= 100) {
-//                 $('.scroll-top').show;
-//             } else {
-//                 $('.scroll-top').hide();;
-//             }
-//         });
-//         $('.scroll-top').click(function () {
-//             $('html,body').animate({
-//                 scrollTop: 0
-//             }, 10000);
-//         });
-//     };
+    var scrollTop = function () {
+        $(window).scroll(function () {
+            if ($(window).scrollTop() >= 100) {
+                $('.scroll-top').show();
+            } else {
+                $('.scroll-top').hide();
+            }
+        });
+        $('.scroll-top').click(function () {
+            $('html,body').animate({
+                scrollTop: 0
+            }, 1000);
+        });
+    };
 
+    var countTo = function () {
+        $('.count').countTo({
+            refreshInterval: 30,
+        });
+        $('.count').click(function () {
+            $(this).countTo({
+                refreshInterval: 30,
+            });
+        })
+    };
 
-//     //Dom Ready
-//     $(function () {
-//         scrollTop();
-//     });
+    //Dom Ready
+    $(function () {
+        scrollTop();
+        countTo();
+    });
 
-
-// })(jQuery);
+})(jQuery);
 
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
